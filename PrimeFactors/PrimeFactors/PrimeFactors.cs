7f483c2f063ea75b67684411c0bf4fb6e7dd838a
seanswalling <sean.swalling.work@gmail.com>
2017-01-11 11:29:12 +0800

    Added 4 Tests

--- PrimeFactors/PrimeFactors/PrimeFactors.cs
+++ PrimeFactors/PrimeFactors/PrimeFactors.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace PrimeFactors
 {
     public class PrimeFactors
     {
+        internal static List<int> Generate(int n)
+        {
+            List<int> Primes = new List<int>();
+            if (n > 1)
+            {
+                Primes.Add(n);
+            }
+            return Primes;
+        }
     }
 }
--- PrimeFactors/PrimeFactors/PrimeFactorsTest.cs
+++ PrimeFactors/PrimeFactors/PrimeFactorsTest.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+
 
 namespace PrimeFactors
 {
+    [TestFixture]
     class PrimeFactorsTest
     {
+        private List<int> List(params int[] ints)
+        {
+            List<int> List = new List<int>();
+            foreach (int i in ints)
+            {
+                List.Add(i);
+            }
+            return List;
+        }
+
+        [Test]
+        public void TestOne()
+        {
+            Assert.AreEqual(List(), PrimeFactors.Generate(1));
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            Assert.AreEqual(List(2), PrimeFactors.Generate(2));
+        }
+
+        [Test]
+        public void TestThree()
+        {
+            Assert.AreEqual(List(3), PrimeFactors.Generate(3));
+        }
+
+        [Test]
+        public void TestFour()
+        {
+            Assert.AreEqual(List(2,2), PrimeFactors.Generate(4));
+        }
     }
+
 }
