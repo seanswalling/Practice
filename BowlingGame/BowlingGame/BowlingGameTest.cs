c7fe634c4438dd1e117b7fabe76197cf9a8e2c02
seanswalling <sean.swalling.work@gmail.com>
2017-01-12 08:40:45 +0800

    Spares Test Passing

--- BowlingGame/BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGame/BowlingGameTest.cs
@@ -10,26 +10,43 @@ namespace BowlingGame
     [TestFixture]
     class BowlingGameTest
     {
+        private Game g;
+        [SetUp]
+        protected void SetUp()
+        {
+            g = new Game();
+        }
+
         [Test]
         public void TestGutterGame()
         {
-            var g = new Game();
-            for (var i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
+            RollMany(20, 0);
             Assert.AreEqual(0, g.Score());
         }
 
         [Test]
         public void TestAllOnes()
         {
-            var g = new Game();
-            for (var i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+            RollMany(20, 1);
             Assert.AreEqual(20, g.Score());
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, g.Score());
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
         }
     }
 }
--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -8,15 +8,31 @@ namespace BowlingGame
 {
     class Game
     {
-        private int _score = 0;
+        private int[] _rolls = new int[21];
+        private int _currentRoll = 0;
         public void Roll(int pins)
         {
-            _score += pins;
+            _rolls[_currentRoll++] = pins;
         }
 
         public int Score()
         {
-            return _score;
+            var score = 0;
+            var frameIndex = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[frameIndex] + _rolls[frameIndex + 1] == 10)
+                {
+                    score += 10 + _rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return score;
         }
     }
 }
