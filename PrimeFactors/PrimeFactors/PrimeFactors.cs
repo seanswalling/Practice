b8a7f400a2ef05ca76be6b7a63b88752d87a1c39
seanswalling <sean.swalling.work@gmail.com>
2017-01-13 09:13:52 +0800

    Prime factors Kata

--- PrimeFactors/PrimeFactors/PrimeFactors.cs
+++ PrimeFactors/PrimeFactors/PrimeFactors.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PrimeFactors
 {
     public class PrimeFactors
     {
+        public static List<int> GeneratePrimes(int number)
+        {
+            var primes = new List<int>();
+            for (var candidate = 2;  number > 1; candidate++)
+            {
+                for(;number % candidate == 0; number /= candidate)
+                {
+                    primes.Add(candidate);
+                }
+            }
+            return primes;
+        }
     }
 }
--- PrimeFactors/PrimeFactors/PrimeFactorsTest.cs
+++ PrimeFactors/PrimeFactors/PrimeFactorsTest.cs
@@ -1,16 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static PrimeFactors.PrimeFactors;
 using NUnit.Framework;
 
 
 namespace PrimeFactors
 {
     [TestFixture]
-    class PrimeFactorsTest
+    internal class PrimeFactorsTest
     {
+        [Test]
+        public void TestOne()
+        {
+            Assert.AreEqual(List(), GeneratePrimes(1));
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            Assert.AreEqual(List(2), GeneratePrimes(2));
+        }
+
+        [Test]
+        public void TestThree()
+        {
+            Assert.AreEqual(List(3), GeneratePrimes(3));
+        }
+
+        [Test]
+        public void TestFour()
+        {
+            Assert.AreEqual(List(2,2), GeneratePrimes(4));
+        }
+
+        [Test]
+        public void TestSix()
+        {
+            Assert.AreEqual(List(2, 3), GeneratePrimes(6));
+        }
+
+        [Test]
+        public void TestEight()
+        {
+            Assert.AreEqual(List(2,2,2), GeneratePrimes(8));
+        }
+
+        [Test]
+        public void TestNine()
+        {
+            Assert.AreEqual(List(3,3), GeneratePrimes(9));
+        }
+        private static List<int> List(params int[] primeFactors)
+        {
+            return primeFactors.ToList();
+        }
     }
 
 }
